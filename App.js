c90b4d640cc06295a5df7ebfe2849aaf076db668
Abhinav Anand <abhi9vanand@gmail.com>
2022-03-23 13:44:53 +0530

    files added

---
+++ App.js
@@ -0,0 +1,25 @@
+import React, {Component} from 'react';
+import { View, Text, Button, NativeModules} from 'react-native';
+//import ToastExample from './ToastExample';
+
+var ToastExample = NativeModules.ToastExample;
+class App extends Component {
+  
+    render() {
+      return (
+          <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#FBD6D2' }}>
+            <Text style={{ fontSize: 20, color: '#A63EC5' }}>
+               React Native Custom Modules{"\n"} 
+              </Text>
+            <Button onPress={() => {
+            ToastExample.show('Awesome!!', ToastExample.SHORT,);
+            
+             }} title="Press This Button"/> 
+
+          </View>
+      );
+  }
+
+}
+
+export default App;
